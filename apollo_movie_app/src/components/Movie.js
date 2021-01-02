4d9ca31097ba5c9619a8f12c6f4c1bbd3a3f4d2b
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2021-01-03 01:34:29 +0900

    added recommends

--- apollo_movie_app/src/components/Movie.js
+++ apollo_movie_app/src/components/Movie.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-
+import "./Movie.css";
 
 const Movie = ({id, poster}) => {
     return (
--- apollo_movie_app/src/routes/Detail.js
+++ apollo_movie_app/src/routes/Detail.js
@@ -2,6 +2,7 @@ import {useParams } from "react-router-dom"
 import {gql} from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
 import "./Details.css";
+import Movie from "../components/Movie";
 
 const GET_MOVIE_DETAILS = gql`
     query getMovieDetails($id : Int!){
@@ -13,8 +14,14 @@ const GET_MOVIE_DETAILS = gql`
             description_full
             medium_cover_image
         }
+        suggestions(id : $id){
+            id
+            medium_cover_image
+        }
     }
 `;
+
+const NO_DESCRIPTION_MESSAGE = "Could not find any information about this movie ;("
 
 export default () => {
     const { id } = useParams();
@@ -23,7 +30,6 @@ export default () => {
             id : parseInt(id)
         }
     })
-    console.log(data);
     return (
         <div className="container container--details">
             {loading && (
@@ -36,18 +42,25 @@ export default () => {
             )}
             {!loading && !error && data && (
                 <div className="container__movie">
-                    <div className="movie__column">
-                        <img className="movie__poster" src={data.movie.medium_cover_image} alt={data.movie.title}></img>
+                    <div className="movie__header">
                         <div className="movie__headline">
-                            <span className="movie__title">{data.movie.title}</span>
+                           <span className="movie__title">{data.movie.title}</span>
                             <span className="movie__year">({data.movie.year})</span>
                         </div>
                         <ul className="movie__genres">
                             {data.movie.genres.map((genre, index) => (<li className="movie__genre" key={index}>{genre}</li>))}
                         </ul>
                     </div>
-                    <div className="movie__column movie__description">{data.movie.description_full}
-                    </div>
+                    <img className="movie__poster" src={data.movie.medium_cover_image} alt={data.movie.title}></img>
+                    <div className="movie__description">{data.movie.description_full ? data.movie.description_full : NO_DESCRIPTION_MESSAGE}</div>
+                    {data.suggestions && (
+                        <div className="movie__recommends">
+                            <p className="recommends__text">Recommends</p>
+                            <div className="recommends__movies">
+                                {data.suggestions.map((movie, index) => <Movie key={index} id={movie.id} poster={movie.medium_cover_image}></Movie>)}
+                            </div>
+                        </div>
+                    )}
                 </div>
             )}
         </div>
